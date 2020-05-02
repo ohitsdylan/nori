900f320ac9812c2bfd791ec0f405abde83f5878c
Dylan <55002846+ohitsdylan@users.noreply.github.com>
2020-05-02 00:10:46 -0400

    finally get node-fetch working
    i hate it
    bring back snekfetch pls

--- commands/booru.js
+++ commands/booru.js
@@ -7,24 +7,28 @@ module.exports = {
 	async execute(message, args) {
 		const fetch = require('node-fetch');
 		const tags = args[0];
+		var body = new Object;
 
 		try {
+			body = await fetch(`https://safebooru.donmai.us/posts.json?random=true&limit=1&tags=${tags}`).then(response => response.json());
 
-			const { body } = await fetch(`https://danbooru.donmai.us/posts.json?random=true&limit=1&tags=${tags}`).then(response => response.json());
+			if (!body.length) {
+				return message.channel.send(`No results came back for ${tags}!`);
+			}
 
-			if (body[0].rating !== 's') {
+			else if (body[0].rating !== 's') {
 				return message.channel.send('L-lewd!');
-			} else
 
-			message.channel.send('Character Tag: ' + body[0].tag_string_character + '\n' +
-			'Artist: ' + body[0].tag_string_artist + '\n' +
-			'Series/Franchise Copyright: ' + body[0].tag_string_copyright + '\n' +
-			body[0].file_url);
-
+			} else {
+				message.channel.send('Character Tag: ' + body[0].tag_string_character + '\n' +
+				'Artist: ' + body[0].tag_string_artist + '\n' +
+				'Series/Franchise Copyright: ' + body[0].tag_string_copyright + '\n' +
+				body[0].file_url);
+			}
 		}
-
 		catch(error) {
+			console.error(error);
 			message.channel.send('I couldn\'t find that!');
 		}
-	},
+	}
 };
