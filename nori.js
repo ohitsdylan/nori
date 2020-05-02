1ecfc7a250ad37b1fefd1606d1cb249756982751
Dylan <55002846+ohitsdylan@users.noreply.github.com>
2020-05-02 00:10:12 -0400

    clean up main bot file
    moved to discord.js v12

--- nori.js
+++ nori.js
@@ -1,14 +1,16 @@
-//Load Discord.JS and establish bot.
+//Load Discord.JS
 const Discord = require('discord.js');
-const bot = new Discord.Client();
-bot.commands = new Discord.Collection();
-const cooldowns = new Discord.Collection();
-const fetch = require('node-fetch');
 
 //Load config file.
 const { prefix, token } = require('./config.json');
 
-//Load commands.
+//Grant life to Nori
+const bot = new Discord.Client();
+
+//Load commands and node modules
+bot.commands = new Discord.Collection();
+const cooldowns = new Discord.Collection();
+const fetch = require('node-fetch');
 const fs = require('fs');
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
@@ -28,7 +30,7 @@ bot.once('ready', () => {
 //Message goodness below.
 bot.on('message', async message => {
 
-	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).split(/ +/g);
 	const commandName = args.shift().toLowerCase();
@@ -36,14 +38,10 @@ bot.on('message', async message => {
 	const command = bot.commands.get(commandName)
 		|| bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-	if (message.content.includes('\<:nickCringe:510507274096082945>')) {
-		message.react('510507274096082945');
-	}
-
 	if (!command) return;
 
 	if (command.args && !args.length) {
-		return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+		let reply = `You didn't provide any arguments, ${message.author}!`;
 
 		if (command.usage) {
 			reply += `\n The manual for \`${prefix}${command.name}\` is: ${command.usage} `;
@@ -60,21 +58,17 @@ bot.on('message', async message => {
 	const timestamps = cooldowns.get(command.name);
 	const cooldownAmount = (command.cooldown) * 1000;
 
-	if (!timestamps.has(message.author.id)) {
-		timestamps.set(message.author.id, now);
-		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-	}
-	else {
-		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+	if (timestamps.has(message.author.id)) {
+	const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
 
 		if (now < expirationTime) {
 			const timeLeft = (expirationTime - now) / 1000;
 			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
 		}
-
-		timestamps.set(message.author.id, now);
-		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 	}
+
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
 	try {
 		command.execute(message, args);
@@ -85,6 +79,8 @@ bot.on('message', async message => {
 	}
 });
 
-process.on('unhandledRejection', error => console.error('Uncaught Promise Rejection', error));
+process.on('unhandledRejection', error => {
+	console.error('Uncaught Promise Rejection:', error);
+});
 
 bot.login(token);
